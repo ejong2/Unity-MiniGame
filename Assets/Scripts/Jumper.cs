985a0a3ef0890868538eae363a2e1ddf2ac6767c
ejong2 <guyus5594@gmail.com>
2023-08-11 15:57:10 +0900

    게임 엔진의 원리

--- Assets/Scripts/Jumper.cs
+++ Assets/Scripts/Jumper.cs
@@ -8,7 +8,6 @@ public class Jumper : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        myRigidbody.AddForce(0, 500, 0);
     }
 
     // Update is called once per frame
