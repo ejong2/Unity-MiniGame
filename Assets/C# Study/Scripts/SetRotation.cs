98a593580751cfb1a8d40d2820a16dc4d0c30703
Lee HyeChang <guyus5594@gmail.com>
2023-08-17 21:38:14 +0900

    회전과 쿼터니언

---
+++ Assets/C# Study/Scripts/SetRotation.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetRotation : MonoBehaviour
+{
+    void Start()
+    {
+        transform.Rotate(30, 0, 0);
+    }
+}
