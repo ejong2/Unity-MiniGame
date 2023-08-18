553cbb3253a93d8c23c15ca693084d4c0c5ba8a0
ejong2 <guyus5594@gmail.com>
2023-08-18 15:17:05 +0900

    인스턴스화

---
+++ Assets/C# Study/Scripts/Spawn.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform spawnPosition;
+    public GameObject target;
+    void Start()
+    {
+        GameObject instance = Instantiate(target, spawnPosition.position, spawnPosition.rotation);
+
+        instance.GetComponent<Rigidbody>().AddForce(0, 1000, 0);
+
+        Debug.Log("Instance name: " + instance.name);
+    }
+}
