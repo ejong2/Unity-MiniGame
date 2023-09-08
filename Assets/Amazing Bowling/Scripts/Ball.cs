dc1c93ce633506815a78659b205012410aaf7010
ejong2 <guyus5594@gmail.com>
2023-09-08 13:43:23 +0900

    Amazing Bowling #3 - 프롭 + 데미지 시스템

--- Assets/Amazing Bowling/Scripts/Ball.cs
+++ Assets/Amazing Bowling/Scripts/Ball.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public LayerMask WhatIsProp;
     public ParticleSystem explosionParticle;
     public AudioSource explosionAudio;
 
     public float maxDamage = 100f;
     public float explosionForce = 1000f;
+    public float explosionRadius = 20f;
     public float lifeTime = 10f;
 
     private void Start()
@@ -16,8 +18,25 @@ public class Ball : MonoBehaviour
         Destroy(gameObject, lifeTime);
     }
 
+    [System.Obsolete]
     private void OnTriggerEnter(Collider other)
     {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius, WhatIsProp);
+        foreach (Collider collider in colliders)
+        {
+            Rigidbody targetRigidbody = collider.GetComponent<Rigidbody>();
+            if (!targetRigidbody)
+            {
+                continue;
+            }
+
+            targetRigidbody.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+
+            Prop targetProp = collider.GetComponent<Prop>();
+            float damage = CalculateDamage(collider.transform.position);
+            targetProp.TakeDamage(damage);
+        }
+
         explosionParticle.transform.parent = null;
 
         explosionParticle.Play();
@@ -25,5 +44,17 @@ public class Ball : MonoBehaviour
 
         Destroy(explosionParticle.gameObject, explosionParticle.duration);
         Destroy(gameObject);
+    }
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        Vector3 explosionToTarget = targetPosition - transform.position;
+        float distance = explosionToTarget.magnitude;
+        float edgeToCenterDistance = explosionRadius - distance;
+        float percentage = edgeToCenterDistance / explosionRadius;
+        float damage = maxDamage * percentage;
+        damage = Mathf.Max(0, damage); 
+
+        return damage;
     }
 }
